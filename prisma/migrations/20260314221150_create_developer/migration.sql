a3ee8029abf028353e7e3e3e38fa3af753d28c1e
Benja-Mad <benjamin.madrid@ug.uchile.cl>
2026-03-14 19:15:16 -0300

    feat: Migración de developer

---
+++ prisma/migrations/20260314221150_create_developer/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Developer" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "seniority" TEXT NOT NULL
+);
